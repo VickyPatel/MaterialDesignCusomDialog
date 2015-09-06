e9645197514432314f076988b36db037d217bf19
Vicky <vicky.7676@yahoo.in>
2015-09-05 22:47:33 -0400

    custom dialog layout implemented

---
+++ app/src/main/java/vickypatel/ca/materialdesigncusomdialog/MainActivity.java
@@ -0,0 +1,112 @@
+package vickypatel.ca.materialdesigncusomdialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button simpleDialog, withoutTittleDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        toolbar.setTitle("Custom Dialogs");
+
+
+        simpleDialog = (Button) findViewById(R.id.simpleDialog);
+        withoutTittleDialog = (Button) findViewById(R.id.withoutTittleDialog);
+
+        simpleDialog.setOnClickListener(this);
+        withoutTittleDialog.setOnClickListener(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //Custom dialog
+        final Dialog dialog = new Dialog(this);
+        // hide to default title for Dialog
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // inflate the layout dialog_layout.xml and set it as contentView
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.custom_dialog, null, false);
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setContentView(view);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+
+        // Retrieve views from the inflated dialog layout and update their values
+        TextView txtTitle = (TextView) dialog.findViewById(R.id.txt_dialog_title);
+        txtTitle.setText("Tittle");
+
+        TextView txtMessage = (TextView) dialog.findViewById(R.id.txt_dialog_message);
+        txtMessage.setText("Content.");
+
+        Button btnOk = (Button) dialog.findViewById(R.id.btn_ok);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        Button btnCancel = (Button) dialog.findViewById(R.id.btn_cancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Close the dialog
+                dialog.dismiss();
+            }
+        });
+
+        // Display the dialog
+        dialog.show();
+
+
+        switch (v.getId()) {
+            case R.id.simpleDialog:
+                txtTitle.setText("Simple Dialog");
+                txtMessage.setText("I am content of simple dialog.");
+                break;
+            case R.id.withoutTittleDialog:
+
+        }
+    }
+}
